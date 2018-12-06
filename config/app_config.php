2387c51472be2873db1f59acd3750af4f0210ce7
abdullahsorathia <abdullahnajib.sorathia@mavs.uta.edu>
2018-12-06 11:31:03 -0600

    security

--- config/app_config.php
+++ config/app_config.php
@@ -1,14 +1,11 @@
 <?php
-    ini_set('display_error',1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL);
     session_start();
    
     class DBConnect{
-        private const SERVERNAME = 'us-cdbr-iron-east-01.cleardb.net';
-        private const USERNAME = 'b149556befae85';
-        private const PASSWORD = '1e01d1608078c94';
-        private const DBNAME = 'heroku_8c7683c880c7ed8';
+        private const SERVERNAME = 'localhost';
+        private const USERNAME = 'root';
+        private const PASSWORD = '';
+        private const DBNAME = 'test';
 
         public static function getDbConnect(){
             $pdo = null;
